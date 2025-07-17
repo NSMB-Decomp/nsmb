9ab929311f77ac28e4447d8f8e4e61a0cb7763b3
GlowingUmbreon <github@umbreon.cc>
2025-07-17 21:20:58 +0100

    .

--- build.zig
+++ build.zig
@@ -68,3 +68,25 @@ pub fn build(b: *std.Build) void {
     const single_step = b.step("single", "");
     single_step.dependOn(&single_cmd.step);
 }
+
+// Old script
+//#!/bin/bash
+//
+//input_file=$(echo "$1" | sed 's|/build/src|/src|; s|\.o$|.cpp|')
+//# I believe the compiler nsmb used is either sp3 or sp4 or some unknown version.
+//wine "$PWD/build/compiler/mwccarm/1.2/sp3/mwccarm.exe" \
+//    $PWD/$input_file -o $PWD/$1 \
+//    -O4,p -interworking -proc arm946e -lang=C++ -Cpp_exceptions off \
+//    -w off -gccinc -nolink -c -RTTI off
+//
+//
+//# -O4,p -enum int -proc arm946e -gccext,on -fp soft -lang c99
+//# -Cpp_exceptions off -gccinc -interworking -gccdep -MD -g
+//# -inline on -str reuse -lang=c -char signed -sym on
+//#echo $output_path> a.txt
+//
+//#wine "/home/umbreon/nsmb/build/compiler/mwccarm/2.0/base/mwccarm.exe" -O4,p \
+//#    -enum int -char signed -str noreuse -proc arm946e -gccext,on -fp soft -inline \
+//#      noauto -lang=C++ -Cpp_exceptions off -RTTI off -interworking -w off \
+//#      -sym on -gccinc -nolink -msgstyle gcc -D_NITRO -D_DEBUG -str reuse  \
+//#      -MD -c ./src/Base.cpp
