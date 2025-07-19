aa56ae9fbd60324f732f18a8f6f888453c77df22
GlowingUmbreon <github@umbreon.cc>
2025-07-20 00:45:03 +0100

    Baase::data - 41%, just vtable messed up?

--- src/AAA.hpp
+++ src/AAA.hpp
@@ -19,10 +19,3 @@ extern void * data_0208b720;
 //
 bool func_0204d82c();
 u32 func_01ffd524(Base*, u32, u32, u32, u32, u32, u32);
-extern u32 data_02085240[2] = {0, 0};
-extern u32 data_02085238[2] = {0, 0};
-extern u32 data_02085228[2] = {0, 0};
-
-extern u32 data_02085230[2] = {0, 0};
-extern u32 data_02085248[2] = {0, 0};
-extern u32 data_02085250[2] = {0, 0};
