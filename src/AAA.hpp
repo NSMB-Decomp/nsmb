59c5b0210f731352a54934a0ae72ffacacd848fa
GlowingUmbreon <github@umbreon.cc>
2025-07-19 11:51:02 +0100

    Base::processCreate - 30%

--- src/AAA.hpp
+++ src/AAA.hpp
@@ -9,3 +9,7 @@ struct ObjectProfile {
 ObjectProfile** CurrentProfileTable;
 
 bool func_0204d82c();
+void func_01ffd524(Base*, u32, u32, u32, u32, u32, u32);
+u32* data_02085240;
+u32* data_02085238;
+u32* data_02085228;
--- src/Base.cpp
+++ src/Base.cpp
@@ -1,7 +1,8 @@
 #include "Base.hpp"
 #include "AAA.hpp"
 
-Base::Base() {
+Base::Base()
+{
   ProcessLink pl = this->process_link;
   pl.connect.object = this;
   pl.update._.prev = (ProcessNode *)0x00;
@@ -21,45 +22,55 @@ Base::Base() {
 bool Base::onCreate() { return true; }
 bool Base::preCreate() { return true; }
 bool Base::onDestroy() { return true; }
-bool Base::preDestroy() {
+bool Base::preDestroy()
+{
   if (
-    !(this->__5 == 0 || 
-    (func_0204d82c() != false) &&
-    this->process_link.connect.firstChild == (SceneNode *)0x0)
-  )
+      !(this->__5 == 0 ||
+        (func_0204d82c() != false) &&
+            this->process_link.connect.firstChild == (SceneNode *)0x0))
   {
     return true;
   }
   return false;
 }
 void Base::pendingDestroy() {}
-void Base::destroy() {
+void Base::destroy()
+{
   bool cond = (this->pending_destroy == false) && this->state == two;
-  if (cond) {
+  if (cond)
+  {
     this->pending_destroy = true;
     this->pendingDestroy();
   }
 }
-Base* Base::getParent() {
+Base *Base::getParent()
+{
   SceneNode *a = this->process_link.connect.parent;
-  if (a) {
+  if (a)
+  {
     return a->object;
-  } else {
+  }
+  else
+  {
     return (Base *)0x0;
   }
 };
 bool Base::onHeapCreated() { return true; }
 void *Base::operator new(u32 count) { return ::operator new(count); }
 void Base::operator delete(void *ptr) { return ::operator delete(ptr); }
-void Base::create() {
-  processCreate();
-  if (this->pending_destroy) {
+void Base::create()
+{
+  // processCreate();
+  if (this->pending_destroy)
+  {
     return;
   }
-  if (this->__1 != 0) {
+  if (this->__1 != 0)
+  {
     return;
   }
-  if (this->state != 0) {
+  if (this->state != 0)
+  {
     return;
   }
   // if (ProcessManager::CurrentTask != Create) {
@@ -68,55 +79,76 @@ void Base::create() {
   // }
   this->__2 = 0x01;
 }
-u32 Base::processDestroy() {
+void Base::processCreate()
+{
+  func_01ffd524(
+    this, 
+    data_02085240[0], 
+    data_02085240[1], 
+    data_02085238[0], 
+    data_02085238[1], 
+    data_02085228[0], 
+    data_02085228[1]
+  );
+}
+u32 Base::processDestroy()
+{
   u16 object_id = this->object_id;
- // u32 ret = process(this,(FunctionPtr *)&PrefetchAbort,1,(FunctionPtr *)&DataAbort,1,
- //                 (FunctionPtr2 *)&NotUsed,1);
- u32 ret = 0;
-  if (ret == 1) {
+  // u32 ret = process(this,(FunctionPtr *)&PrefetchAbort,1,(FunctionPtr *)&DataAbort,1,
+  //                 (FunctionPtr2 *)&NotUsed,1);
+  u32 ret = 0;
+  if (ret == 1)
+  {
     unloadSceneOverlay();
   }
   return ret;
 }
-bool Base::hasChildNotCreated() {
-  SceneNode* next = this->process_link.connect.func_020439f0();
-  SceneNode* cur;
+bool Base::hasChildNotCreated()
+{
+  SceneNode *next = this->process_link.connect.func_020439f0();
+  SceneNode *cur;
 
-  //while (cur != (SceneNode *)0x0 && next != cur) {
-  //  if ((*cur->object).state == zero) {
-  //    return true;
-  //  }
-  //  cur = cur->func_02043a2c();
-  //}
+  // while (cur != (SceneNode *)0x0 && next != cur) {
+  //   if ((*cur->object).state == zero) {
+  //     return true;
+  //   }
+  //   cur = cur->func_02043a2c();
+  // }
 
-  for (cur = this->process_link.connect.firstChild; 
-      (cur != (SceneNode *)0x0 && (cur != next));
-       cur = cur->func_02043a2c()) {
-    if ((*cur->object).state == zero) {
+  for (cur = this->process_link.connect.firstChild;
+       (cur != (SceneNode *)0x0 && (cur != next));
+       cur = cur->func_02043a2c())
+  {
+    if ((*cur->object).state == zero)
+    {
       return true;
     }
   }
-  
+
   return false;
 }
-void Base::setSpawnParams(u16 a, ProcessLink* b, u32 c, u8 d) {
+void Base::setSpawnParams(u16 a, ProcessLink *b, u32 c, u8 d)
+{
   Base::SpawnParam3 = c;
   Base::SpawnParam1 = a;
   Base::SpawnParam4 = d;
   Base::SpawnParam2 = b;
 }
-Base* Base::spawn(u16 overlay_id, ProcessLink* b, u32 c, u8 d) {
+Base *Base::spawn(u16 overlay_id, ProcessLink *b, u32 c, u8 d)
+{
   Base::data_0208faf0 = overlay_id;
   Base::data_0208fae8 = 1;
   u32 new_overlay = Base::loadSceneOverlay(overlay_id);
-  if (new_overlay == 3) {
+  if (new_overlay == 3)
+  {
     return (Base *)0x00;
   }
   Base::data_0208fae8 = 2;
   setSpawnParams(overlay_id, b, c, d);
   Base::data_0208fae8 = 3;
-  Base* ret = (CurrentProfileTable)[overlay_id]->constructor();
-  if (ret == (Base*)0x00) {
+  Base *ret = (CurrentProfileTable)[overlay_id]->constructor();
+  if (ret == (Base *)0x00)
+  {
     Base::data_0208faf0 = 0xFFFF;
     Base::data_0208fae8 = 0;
     return ret;
@@ -127,24 +159,33 @@ Base* Base::spawn(u16 overlay_id, ProcessLink* b, u32 c, u8 d) {
   Base::data_0208faf0 = 0xFFFF;
   return ret;
 }
-u32 Base::loadSceneOverlay(u16 a) {
-  if (Base::data_0208fafc != 0x00) {
+u32 Base::loadSceneOverlay(u16 a)
+{
+  if (Base::data_0208fafc != 0x00)
+  {
     ((void (*)(u16))Base::data_0208fafc)(a);
-  } else {
+  }
+  else
+  {
     return 2;
   }
 }
-void Base::unloadSceneOverlay() { // Not sure if the arg this accepts is "this" or arg1
-  if (Base::data_0208fb00 != 0x00) {
-    ((void (*)(Base*))Base::data_0208fb00)(this);
+void Base::unloadSceneOverlay()
+{ // Not sure if the arg this accepts is "this" or arg1
+  if (Base::data_0208fb00 != 0x00)
+  {
+    ((void (*)(Base *))Base::data_0208fb00)(this);
   }
 }
-Base* Base::spawnChild(u16 overlay_id, Base* b, u32 c, u8 d) {
-  if (b != (Base *)0x00) {
+Base *Base::spawnChild(u16 overlay_id, Base *b, u32 c, u8 d)
+{
+  if (b != (Base *)0x00)
+  {
     return b->spawn(overlay_id, &b->process_link, c, d);
   }
   return (Base *)0x00;
 }
-Base* Base::spawnParent(u16 a, u32 b, u8 c) {
+Base *Base::spawnParent(u16 a, u32 b, u8 c)
+{
   return Base::spawn(a, 0, b, c);
 }
