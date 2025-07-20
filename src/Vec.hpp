91d6f68658c9f802df4b86fe150b79ba0cd1f6dc
GlowingUmbreon <github@umbreon.cc>
2025-07-20 12:36:45 +0100

    Temporary Vec change

--- src/Vec.hpp
+++ src/Vec.hpp
@@ -1,12 +1,13 @@
 class Vec3 {
 public:
+    u32 fake_vtable; // To keep data sizes correct in Actor.hpp
     i32 x;
     i32 y;
     i32 z;
 
     void sub(Vec3 a, Vec3 b);
 
-    virtual ~Vec3();
+    //virtual ~Vec3();
 };
 
 class Vec3s {
