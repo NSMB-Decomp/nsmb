ce02dc6788d7b29dee78f880ed726e41a5d9a242
GlowingUmbreon <github@umbreon.cc>
2025-07-27 16:48:35 +0100

    Seperated out Nitro

---
+++ src/Nitro/Nitro.hpp
@@ -0,0 +1,43 @@
+#include "../base_types.hpp"
+
+// TODO: Seperate out files.
+namespace Nitro
+{
+    namespace Tick
+    {
+        void Init();
+    }
+    namespace Timer
+    {
+        void Init();
+    }
+    namespace MultiThread
+    {
+        void Init();
+    }
+    namespace Exception
+    {
+        void SetHandler(u32, u32);
+    }
+    namespace Graphics
+    {
+        void ResetOAM();
+        bool Step();
+        inline void SwapBuffer(u32 a, u32 b) {
+        u32 *GFX_FIFO_SWAP_BUFFERS = (u32 *)0x04000540;
+        *GFX_FIFO_SWAP_BUFFERS = (a << 1) | b;
+        }
+    }
+    namespace Math {
+        void __stub();
+    }
+    void func_02061588(u32);
+    void EnableDisplay();
+    void func_01ff8128(u32);
+    void func_01ffa5ec(u32, u16);
+    void func_02060e38(u32);
+    void func_0206d554();
+    void MainInit();
+    void func_0206f3cc(void(*)());
+    void func_02063fd4();
+}
--- src/init.cpp
+++ src/init.cpp
@@ -1,21 +1,7 @@
+#include "Nitro/Nitro.hpp"
 #include "base_types.hpp"
-void Nitro_Tick_Init();
-void Nitro_Timer_Init();
-void Nitro_MultiTHread_init();
-void Nitro_Exception_SetHandler(u32, u32);
-void Nitro_EnableDisplay();
-void Nitro_Graphics_ResetOAM();
-bool Nitro_Graphics_Step();
-void Nitro_func_02063fd4();
-void Nitro_func_01ff8128(u32);
-void Nitro_func_01ffa5ec(u32, u16);
-void Nitro_func_02060e38(u32);
-void Nitro_Math___stub();
-void Nitro_func_0206d554();
-void Nitro_MainInit();
-void func_0204c7f4(u32, void *);
-void Nitro_func_0206f3cc(void(*)());
 
+void func_0204c7f4(u32, void *);
 void func_02005098();
 void SystemThreadsSleep();
 void func_02005230();
@@ -55,7 +41,6 @@ void func_02043d10();
 void func_0202187c();
 void func_020133a4();
 void func_01ff9010();
-void Nitro_func_02061588(u32);
 void func_0200e040();
 void func_02014a58();
 void __stub_1();
@@ -77,26 +62,21 @@ void (*data_0203981c)();
 void (*data_02039820)();
 void (*data_02039824)();
 
-inline void Nitro_Graphics_SwapBuffer(u32 a, u32 b) {
-  u32 *GFX_FIFO_SWAP_BUFFERS = (u32 *)0x04000540;
-  *GFX_FIFO_SWAP_BUFFERS = (a << 1) | b;
-}
-
 char GAME_NAME[8] = "Mario2d";
 void InitGame() {
   u16 *REG_IME = (u16 *)0x04000208;
 
-  Nitro_func_02063fd4();
+  Nitro::func_02063fd4();
   func_02005ab0();
-  Nitro_func_01ff8128(1);
+  Nitro::func_01ff8128(1);
   u16 ime = *REG_IME;
   *REG_IME = 1;
-  Nitro_func_01ffa5ec(1, ime);
-  Nitro_func_02060e38(1);
+  Nitro::func_01ffa5ec(1, ime);
+  Nitro::func_02060e38(1);
   bool is_multiboot_cart = Wifi_isMultiBootCart();
-  Nitro_Math___stub();
+  Nitro::Math::__stub();
   func_020046d8();
-  Nitro_func_0206d554();
+  Nitro::func_0206d554();
   func_0204c7f4(0xd01, GAME_NAME);
   func_02013100();
   FS_init();
@@ -125,7 +105,7 @@ void InitGame() {
   func_0202187c();
   func_020133a4();
   func_01ff9010();
-  Nitro_func_02061588(3);
+  Nitro::func_02061588(3);
   u32 *GFX_FIFO_LIGHT_DIRECTION = (u32 *)0x040004c8;
   *GFX_FIFO_LIGHT_DIRECTION = 0x296a5800;
   u32 *GFX_FIFO_LIGHT_COLOR = (u32 *)0x040004cc;
@@ -149,14 +129,14 @@ void StartGameLoop() {
   u32 *GFX_FIFO_SWAP_BUFFERS = (u32 *)0x04000540;
 
   SystemThreadsSleep();
-  Nitro_EnableDisplay();
+  Nitro::EnableDisplay();
 
   do {
     func_02005230();
     func_02021654();
     func_0200a594();
-    Nitro_Graphics_ResetOAM();
-    bool rendered_frame = Nitro_Graphics_Step();
+    Nitro::Graphics::ResetOAM();
+    bool rendered_frame = Nitro::Graphics::Step();
     if (rendered_frame) {
       func_020132a0();
       ProcessManager_ExecuteTasks();
@@ -164,7 +144,7 @@ void StartGameLoop() {
     }
     FrameCounter += 1;
     func_01ff9010();
-    Nitro_Graphics_SwapBuffer(data_02085a78, data_02085a74);
+    Nitro::Graphics::SwapBuffer(data_02085a78, data_02085a74);
     func_0201031c();
     func_020058c0();
     func_02010094();
@@ -178,10 +158,10 @@ void InitNitroMainGame() {
   // Temp values
   u32 a = 0x1234; // arm9_mainCODE
   u32 b = 0x4321; // autoload_3.bss
-  Nitro_Tick_Init();
-  Nitro_Timer_Init();
-  Nitro_MultiTHread_init();
-  Nitro_Exception_SetHandler(a, b);
+  Nitro::Tick::Init();
+  Nitro::Timer::Init();
+  Nitro::MultiThread::Init();
+  Nitro::Exception::SetHandler(a, b);
 }
 
 void InitMainGame() {
@@ -191,9 +171,9 @@ void InitMainGame() {
 }
 
 void SharedGameInit() {
-  Nitro_MainInit();
+  Nitro::MainInit();
   __stub_3();
-  Nitro_func_0206f3cc(*func_02005098);
+  Nitro::func_0206f3cc(*func_02005098);
   data_020850e8 = 1;
   func_020455d8();
   __stub_4();
