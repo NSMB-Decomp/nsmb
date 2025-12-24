85a0da8f47ac66ea094b96e642e154dab8100a28
GlowingUmbreon <github@umbreon.cc>
2025-12-24 15:10:15 +0000

    StageActor::preUpdate() - 19%

--- src/Bases/StageActor.cpp
+++ src/Bases/StageActor.cpp
@@ -2,4 +2,17 @@
 
 StageActor::~StageActor() {
     ActiveCollider::delink((void*)&this->c);
-};
+}
+
+bool StageActor::preUpdate() {
+    bool a = Actor::preUpdate();
+    if (a == false) {
+        return false;
+    }
+
+    return false;
+}
+
+void StageActor::postUpdate() {
+    Actor::postUpdate();
+}
