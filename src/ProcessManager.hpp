10847cdc4a78ad743049602965ac4865444f723d
GlowingUmbreon <github@umbreon.cc>
2025-12-24 13:45:10 +0000

    Added back Extern to ProcessManager temporarily

--- src/ProcessManager.hpp
+++ src/ProcessManager.hpp
@@ -63,14 +63,15 @@ enum ProcessType {
 };
 
 namespace ProcessManager {
-    u32 CurrentTask;
-    ProcessList CreateTask;
-    ProcessList ConnectTask;
-    ProcessList ExecuteTask;
-    ProcessList DrawTask;
-    ProcessList DestroyTask;
+    // TODO: Switch these from extern
+    extern u32 CurrentTask;
+    extern ProcessList CreateTask;
+    extern ProcessList ConnectTask;
+    extern ProcessList ExecuteTask;
+    extern ProcessList DrawTask;
+    extern ProcessList DestroyTask;
 
-    ProcessList idLookupProcesses[8];
+    extern ProcessList idLookupProcesses[8];
     u32 getIDIndex(ProcessLink*);
     Base* getNextObjectByObjectID(u8 class_id, Base* prev);
 }
