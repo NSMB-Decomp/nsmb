a04fe7252ae2163278d712caa093e957793ca764
GlowingUmbreon <github@umbreon.cc>
2025-12-31 12:15:25 +0000

    Base::func_01ffd290() - 98%

--- src/Bases/Base_itcm.cpp
+++ src/Bases/Base_itcm.cpp
@@ -1,11 +1,12 @@
 #include "Base.hpp"
 
-i32 Base::func_01ffd524(PTMF a, PTMF b, PTMF c) {
-    void** vtable = *((void***)this);
-    //if ((b.params & 1) != 0) {
-    //    b.func = (void(*)())vtable[b.params];
-    //}
-    //b.func();
+i32 Base::func_01ffd524(PTMF a, PTMF b, PTMF c)
+{
+    void **vtable = *((void ***)this);
+    // if ((b.params & 1) != 0) {
+    //     b.func = (void(*)())vtable[b.params];
+    // }
+    // b.func();
 }
 
 bool Base::onUpdate()
@@ -45,7 +46,7 @@ bool Base::func_01ffd290()
     if (this->pending_destroy)
     {
         this->pending_destroy = false;
-        if ((bool)(this->state != Active) ? 0 : 1)
+        if ((BOOL)(this->state == Active) != FALSE)
         {
             LinkedList_Remove(&ProcessManager::ExecuteTask, &this->process_link.update);
             LinkedList_Remove(&ProcessManager::DrawTask, &this->process_link.render);
@@ -70,63 +71,73 @@ bool Base::func_01ffd290()
             // Check to see if this object should be updated
             u32 skip_flags = parent->skipFlags;
             if (
-                (skip_flags & Update) == 0 && 
-                (skip_flags & UpdateChildren) != 0)
+                ((skip_flags & UpdateChildren) != 0) ||
+                ((skip_flags & Update) != 0))
+            {
+
+                this->skipFlags = (SkipFlags)(this->skipFlags | Update);
+            }
+            else
             {
                 if ((this->skipFlags & Update) != 0)
                 {
                     this->skipFlags = (SkipFlags)(this->skipFlags & ~Update);
                 }
-                else
-                {
-                    this->skipFlags = (SkipFlags)(this->skipFlags | Update);
-                }
             }
+
+            skip_flags = parent->skipFlags;
             // Check to see if this object should be rendered
-            if (((skip_flags & Render) == 0) && ((skip_flags & RenderChildren) == 0))
+            if (
+                ((skip_flags & RenderChildren) != 0) ||
+                ((skip_flags & Render) != 0))
+            {
+
+                this->skipFlags = (SkipFlags)(this->skipFlags | Render);
+            }
+            else
             {
                 if ((this->skipFlags & Render) != 0)
                 {
-                    this->skipFlags = (SkipFlags)(this->skipFlags & ~Render);
-                }
-                else
-                {
-                    this->skipFlags = (SkipFlags)(this->skipFlags | Render);
+                    this->skipFlags = this->skipFlags & ~Render;
                 }
             }
-            //
-            state = this->state;
-            if (state == Active)
+        }
+        //
+        state = this->state;
+        if (BOOL(state == Active) != FALSE)
+        {
+            PriorityNode *u = &this->process_link.update;
+            if (BOOL(u->sortPriority != u->currentPriority) != FALSE)
             {
-                if (this->process_link.update.sortPriority != this->process_link.update.currentPriority)
+                LinkedList_Remove(&ProcessManager::ExecuteTask, &this->process_link.update);
+                PriorityNode *update = &this->process_link.update;
+                update->currentPriority = update->sortPriority;
+                ProcessSet_add(&ProcessManager::ExecuteTask, update);
+            }
+            PriorityNode *r = &this->process_link.render;
+            if (BOOL(r->sortPriority != r->currentPriority) != FALSE)
+            {
+                LinkedList_Remove(&ProcessManager::DrawTask, &this->process_link.render);
+                PriorityNode *render = &this->process_link.render;
+                render->currentPriority = render->sortPriority;
+                ProcessSet_add(&ProcessManager::DrawTask, render);
+            }
+        }
+        else if (BOOL(state == Inactive) == FALSE)
+        {
+            if (this->pending_create)
+            {
+                this->pending_create = 0;
+                LinkedList_append(&ProcessManager::CreateTask, &this->process_link.update);
+            }
+            else
+            {
+                if (this->pending_update)
                 {
-                    LinkedList_Remove(&ProcessManager::ExecuteTask, &this->process_link.update);
-                    this->process_link.update.currentPriority = this->process_link.update.sortPriority;
+                    this->pending_update = false;
                     ProcessSet_add(&ProcessManager::ExecuteTask, &this->process_link.update);
-                }
-                if (this->process_link.render.sortPriority != this->process_link.render.currentPriority)
-                {
-                    LinkedList_Remove(&ProcessManager::DrawTask, &this->process_link.render);
-                    this->process_link.render.currentPriority = this->process_link.render.sortPriority;
                     ProcessSet_add(&ProcessManager::DrawTask, &this->process_link.render);
-                }
-            }
-            else if (state == Inactive)
-            {
-                if (this->pending_create)
-                {
-                    if (this->pending_update)
-                    {
-                        this->pending_update = false;
-                        ProcessSet_add(&ProcessManager::ExecuteTask, &this->process_link.update);
-                        ProcessSet_add(&ProcessManager::DrawTask, &this->process_link.render);
-                        this->state = Active;
-                    }
-                }
-                else
-                {
-                    this->pending_create = 0;
-                    LinkedList_append(&ProcessManager::CreateTask, &this->process_link.update);
+                    this->state = Active;
                 }
             }
         }
@@ -134,9 +145,10 @@ bool Base::func_01ffd290()
     return true;
 }
 
-void Base::func_01ffd22c() {
+void Base::func_01ffd22c()
+{
     PTMF a = {0x18, 1};
     PTMF b = {0x1c, 1};
     PTMF c = {0x20, 1};
-    Base::func_01ffd524(a,b,c);
+    Base::func_01ffd524(a, b, c);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ void main() {
     game_mode = (u8)(func_020109c8()) != (u8) 0;
     boot_scene = 0;
   } else {
-    game_mode = *DAT_0200306c & 0xf;
+    game_mode = *DAT_0200306c & 0xf00;
     boot_scene = (*DAT_0200306c) >> 8;
   };
   u32 a = (*DAT_0200306c & 0xf0000) >> 0x10;
