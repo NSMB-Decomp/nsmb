b856d3cb015e993c5dfa4e58400b50afcf0e58ca
GlowingUmbreon <github@umbreon.cc>
2026-02-23 21:22:31 +0000

    Actor_22 - 100%

---
+++ src/Bases/Actor_22.cpp
@@ -0,0 +1,8 @@
+#include "Actor_22.hpp"
+
+Actor_22::~Actor_22() {}
+
+void *Actor_22::create()
+{
+    return new Actor_22();
+}
---
+++ src/Bases/Actor_22.hpp
@@ -0,0 +1,9 @@
+#include "StageEntity.hpp"
+
+class Actor_22 : StageEntity {
+       
+    inline Actor_22();
+    ~Actor_22();
+    static void* create();
+};
+inline Actor_22::Actor_22() {}
