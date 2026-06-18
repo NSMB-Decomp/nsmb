b41e68db3760a4b0e7950c07ee333721d3b115bb
Ed-1T <wmusicwtf@gmail.com>
2026-06-18 00:01:21 -0400

    WmController::canEntityTransitLink() - 100%

--- src/worldmap/controller.cpp
+++ src/worldmap/controller.cpp
@@ -4,3 +4,16 @@ s32 WmController::getEntityMoveIndex(u32 entityIndex, u32 moveCount, u32 moveSte
 	return (entityIndex * 12 + moveCount) * 2 + moveStep;
 }
 
+bool WmController::canEntityTransitLink(WM::NodeLink* link) {
+
+	if (link->flag & LF_Pipe)
+		return false;
+
+	u8 ps = save.game.pathStates[save.game.currentWorld][link->path];
+	if (u8(ps & PS_Unk40) == 0 && u8(wxPaths[link->path].flag & PF_Unk1))
+		return false;
+
+	return true;
+
+}
+
