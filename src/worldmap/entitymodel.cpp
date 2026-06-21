06b906e9236c1421b595b6efba8a30d7ab5adce4
Ed-1T <wmusicwtf@gmail.com>
2026-06-21 14:19:02 -0400

    Add WmEntityModel stub

--- src/worldmap/entitymodel.cpp
+++ src/worldmap/entitymodel.cpp
@@ -1,1 +1,36 @@
 #include "entitymodel.hpp"
+
+using namespace WM;
+
+
+WmEntityModel::WmEntityModel() {}
+
+WmEntityModel::~WmEntityModel() {}
+
+
+bool WmEntityModel::create(u32 type) {
+	return false;
+}
+
+void WmEntityModel::render(Mat4x3* mtx, Vec3_32* scale) {
+
+}
+
+void WmEntityModel::update() {
+
+	model.update();
+
+	if (type == ET_FlyingBlock) {
+		shadowAnim.update();
+	}
+
+}
+
+void WmEntityModel::init(u32 animID) {
+
+}
+
+
+bool WmEntityModel::loadResources() {
+	return false;
+}
--- src/worldmap/entitymodel.hpp
+++ src/worldmap/entitymodel.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "scene.hpp"
 #include "../graphics/3d/modelanm.hpp"
 #include "../graphics/3d/animationctrl.hpp"
 
@@ -10,7 +11,21 @@ public:
 		u32 animFile;
 	};
 
+	WmEntityModel();
+
 	virtual ~WmEntityModel();
+
+
+	bool create(u32 type);
+
+	void render(Mat4x3* mtx, Vec3_32* scale);
+
+	void update();
+
+	void init(u32 animID);
+
+
+	static bool loadResources();
 
 
 	ModelAnm model;
