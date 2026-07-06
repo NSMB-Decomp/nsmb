2524e689417d6f10b128fb7b626c3983beddd048
GlowingUmbreon <github@umbreon.cc>
2026-07-06 20:39:23 +0100

    DebugScene::func_ov002_020cc514() - 97%

--- src/Scenes/DebugScene.cpp
+++ src/Scenes/DebugScene.cpp
@@ -13,7 +13,7 @@ const SomethingElse data_ov002_020cd11c[] = {
     {"KINOK", 0x9, 0x9, 0xA000}, {"KINO2", 0x9, 0x9, 0xC000}, {"KINO3", 0x9, 0x9, 0xA001}, {"WORLD", 0x8, 0x8, 0x0}, {"SAVE", 0x7, 0x7, 0x0}, {"TITLE", 0x4, 0x4, 0x0}, {"ENDIN", 0xC, 0xC, 0x0}, {"CRSIN", 0xD, 0xD, 0x0}, {"GOVER", 0xE, 0xE, 0x0}, {"KEY", 0x11, 0x11, 0x0}, {"SOUND", 0x10, 0x10, 0x0}, {"ENTRY", 0x6, 0x6, 0x0}, {"ERROR", 0xB, 0xB, 0x0}, {"MGVS", 0x146, 0x146, 0x0},
 };
 u8 data_ov002_020cd220[4] = {1, 1, 3, 0};
-
+const u8 data_ov002_020cd114[4] = {0, 1, 3, 0};
 
 DebugScene::DebugScene()
 {
@@ -419,18 +419,16 @@ void DebugScene::func_ov002_020cc514()
 	u32 r4;
 	extern u8 data_ov002_020cd240[2][2];
 	if (data_ov002_020cd304) {
-				i32 iVar7 = data_ov002_020cd304 - 1;
+		i32 iVar7 = data_ov002_020cd304 - 1;
 		r6 = data_ov002_020cd220[iVar7];
 		r5 = data_ov002_020cd240[iVar7][0];
 		r4 = data_ov002_020cd240[iVar7][1];
-
 	} else {
 		r6 = 1;
 		r5 = 0;
 		r4 = 1;
 	}
 
-	
 	bool bVar4 = this->_1e8 == ~1;
 	if (bVar4) {
 		this->_1e8 = -1;
@@ -441,14 +439,15 @@ void DebugScene::func_ov002_020cc514()
 		data_02085a1c = 0;
 	}
 
-	data_02085a10 = this->_1dc;
+	data_02085a10 = this->_220;
 
-	struct Somettting {
-		u8 *DAT_arm9_ov002__020cd064;
-		u8 DAT_arm9_ov002__020cd090[2];
-	};
-	extern Somettting data_ov002_020cd114;
-	extern u32 func_0200696c__(u32, u16, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8, u8);
+	//struct Somettting {
+	//	u8 *DAT_arm9_ov002__020cd064;
+	//	u8 DAT_arm9_ov002__020cd090[2];
+	//};
+	//extern Somettting data_ov002_020cd114;
+	
+	extern u32 func_0200696c__(u32, u16, u8, u8, u8, u32, u32, u32, u32, u8, u8, u8, u8, u8, u8, u8, u32);
 	func_0200696c__(
 		0xd, 
 		0, 
@@ -457,11 +456,11 @@ void DebugScene::func_ov002_020cc514()
 		this->_1e4, 
 		0, 
 		r6, 
-		r5,
+		r5, 
 		r4, 
-		*data_ov002_020cd114.DAT_arm9_ov002__020cd064, 
+		data_ov002_020cd300, 
 		this->_1e8, 
-		data_ov002_020cd220[this->_1ec], 
+		data_ov002_020cd114[this->_1ec], 
 		this->_1f0, 
 		this->_20c, 
 		this->_210, 
@@ -471,17 +470,17 @@ void DebugScene::func_ov002_020cc514()
 
 	u32 data_02088e04 = 0;
 	extern Save save;
-	switch (this->_1e8) {
+	switch (this->_22c) {
 	case 0:
 		save.game.mapEntities[0][0].node = 0xff;
 		save.game.mapEntities[0][1].node = 0xff;
 		break;
 	case 1:
-		save.game.currentWorld = 0;
+		save.game.currentWorld = 0; 
 		save.game.currentWorldNode = 0;
 		save.game.mapEntities[0][0].node = 0x00;
 		save.game.mapEntities[0][1].node = 0xff;
-		save.game.mapEntities[0][1].type = 0;
+		save.game.mapEntities[0][0].type = 0;
 		break;
 	case 2:
 		save.game.currentWorld = 0;
@@ -567,7 +566,7 @@ DebugScene::~DebugScene()
 {
 }
 
-const u8 data_ov002_020cd114[4] = {0, 1, 3, 0};
+
 u8 data_ov002_020cd240[4][2] = {
     {0, 0},
     {1, 0},
@@ -577,4 +576,3 @@ u8 data_ov002_020cd240[4][2] = {
 ObjectProfile DebugScene_Profile = {DebugScene::create, 2, 9};
 
 void (DebugScene::*data_ov002_020cd308[])() = {DebugScene::func_ov002_020cc514, DebugScene::func_ov002_020cc328};
-
