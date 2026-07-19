9e278919a4a921348da21c21bea558e770cdb5bc
GlowingUmbreon <github@umbreon.cc>
2026-07-19 02:03:26 +0100

    FireSnake::loadResources() - 100%

--- src/Bases/FireSnake.cpp
+++ src/Bases/FireSnake.cpp
@@ -16,3 +16,11 @@ void *FireSnake::create_unused()
 {
 	return new FireSnake();
 }
+
+bool FireSnake::loadResources() {
+	FS::Cache::loadFile(0x4fe, 0x0);
+	FS::Cache::loadFile(0x4ff, 0x0);
+	FS::Cache::loadFile(0x4fc, 0x0);
+	FS::Cache::loadFile(0x4fd, 0x0);
+	return true;
+}
