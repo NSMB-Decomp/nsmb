ca95ac4bde12258d0675f6a4a7c1de89c96075b7
GlowingUmbreon <github@umbreon.cc>
2026-08-09 13:05:46 +0100

    Object306::create() - 100%

--- src/Bases/stubs/Object306.hpp
+++ src/Bases/stubs/Object306.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include "../StageEntity.hpp"
+#include "../../graphics/3d/modelanm.hpp"
 #include "../../AAA.hpp"
 
 // MainProfileTable slot 306  |  ov009  |  profile @ 0x020dac58
-class Object306 : public StageEntity {
+class Object306 : public Actor {
 public:
+	ModelAnm _120;
+	u8 _pad0[0x1c];
 	static void *create();
 	inline Object306() {};
 	inline ~Object306() {};
