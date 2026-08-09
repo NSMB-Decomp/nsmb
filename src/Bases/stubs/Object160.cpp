3aab7abead408e5a3934323a430f33881fd119b7
GlowingUmbreon <github@umbreon.cc>
2026-08-09 17:12:09 +0100

    Object160::create() - 100%

--- src/Bases/stubs/Object160.cpp
+++ src/Bases/stubs/Object160.cpp
@@ -5,5 +5,16 @@ void *Object160::create()
 	return new Object160();
 }
 
+s32 Object160::onCreate() {}
+s32 Object160::onDestroy() {}
+s32 Object160::onUpdate() {}
+s32 Object160::onRender() {}
+
+Object160_Unknown::Object160_Unknown() {}
+
+Object160::Object160() {
+
+}
+
 // 0x0216e2b0
-ActorProfile Object160_Profile = { Object160::create, 160, 6, NULL /* TODO: 0x0215f974 */ };
+ObjectProfile Object160_Profile = { Object160::create, 160, 6 };
--- src/Bases/stubs/Object160.hpp
+++ src/Bases/stubs/Object160.hpp
@@ -2,13 +2,32 @@
 #include "../StageEntity.hpp"
 #include "../../AAA.hpp"
 
-// MainProfileTable slot 160  |  ov054  |  profile @ 0x0216e2b0
-class Object160 : public StageEntity {
-public:
-	static void *create();
-	inline Object160() {};
-	inline ~Object160() {};
+class Object160_Unknown {
+	public:
+	u8 _pad0[0x4];
+
+	Object160_Unknown();
+	~Object160_Unknown();
 };
 
-extern ActorProfile Object160_Profile;
+// MainProfileTable slot 160  |  ov054  |  profile @ 0x0216e2b0
+class Object160 : public Object {
+public:
+	u8 _pad0[0x8];
+	Object160_Unknown _64[5];
+	u8 _pad1[0x2c];
+
+	static void *create();
+	Object160();
+	inline ~Object160() {};
+
+	void func_ov054_0215fea4();
+
+	virtual s32 onCreate();
+	virtual s32 onDestroy();
+	virtual s32 onUpdate();
+	virtual s32 onRender();
+};
+
+extern ObjectProfile Object160_Profile;
 
