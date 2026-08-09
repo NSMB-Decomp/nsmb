d73fbf04acc43c612b645206b3d256a7f4a11d0c
GlowingUmbreon <github@umbreon.cc>
2026-08-09 15:19:20 +0100

    Object150::create() - 100%

--- src/Bases/stubs/Object150.cpp
+++ src/Bases/stubs/Object150.cpp
@@ -4,6 +4,12 @@ void *Object150::create()
 {
 	return new Object150();
 }
+bool Object150::onHeapCreated() {}
+s32 Object150::Object150::onCreate() {}
+bool Object150::onUpdate_0() {}
+s32 Object150::onRender() {}
+void Object150::pendingDestroy() {}
+s32 Object150::onDestroy() {}
 
 // 0x02173680
 ActorProfile Object150_Profile = { Object150::create, 150, 174, NULL /* TODO: 0x02172b18 */ };
--- src/Bases/stubs/Object150.hpp
+++ src/Bases/stubs/Object150.hpp
@@ -1,13 +1,24 @@
 #pragma once
 #include "../StageEntity.hpp"
 #include "../../AAA.hpp"
+#include "../../graphics/3d/modelanm.hpp"
 
 // MainProfileTable slot 150  |  ov058  |  profile @ 0x02173680
 class Object150 : public StageEntity {
 public:
+	ModelAnm model;
+	u8 _pad0[0x1c];
+
 	static void *create();
 	inline Object150() {};
 	inline ~Object150() {};
+
+	virtual s32 onCreate();
+	virtual s32 onDestroy();
+	virtual s32 onRender();
+	virtual void pendingDestroy();
+	virtual bool onHeapCreated();
+	virtual bool onUpdate_0();
 };
 
 extern ActorProfile Object150_Profile;
