15064a0f772353b2e805ea3975c9b7032810dc0f
GlowingUmbreon <github@umbreon.cc>
2026-08-09 14:15:34 +0100

    Object313::Object313() - 100%

--- src/Bases/stubs/Object313.cpp
+++ src/Bases/stubs/Object313.cpp
@@ -5,5 +5,9 @@ void *Object313::create()
 	return new Object313();
 }
 
+Object313::Object313() {
+	this->_1c8 = 0;
+}
+
 // 0x020db134
 ActorProfile Object313_Profile = { Object313::create, 313, 304, NULL /* TODO: 0x020d929c */ };
--- src/Bases/stubs/Object313.hpp
+++ src/Bases/stubs/Object313.hpp
@@ -1,12 +1,25 @@
 #pragma once
 #include "../StageEntity.hpp"
 #include "../../AAA.hpp"
+#include "../../graphics/3d//modelanm.hpp"
+
+class Object313_Unknwon {
+	public:
+	u8 _pad[0x14];
+	Vec3_32 _14;
+	Object313_Unknwon();
+	~Object313_Unknwon();
+};
 
 // MainProfileTable slot 313  |  ov009  |  profile @ 0x020db134
-class Object313 : public StageEntity {
+class Object313 : public Actor {
 public:
+	ModelAnm model;
+	u32 _1c8;
+	Object313_Unknwon _____[2];
+
 	static void *create();
-	inline Object313() {};
+	Object313();
 	inline ~Object313() {};
 };
 
