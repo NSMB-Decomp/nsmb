23be4b49ba081ebadbb35139584656b29a163912
GlowingUmbreon <github@umbreon.cc>
2026-08-09 13:14:55 +0100

    Object311::create() - 100%

--- src/Bases/stubs/Object311.hpp
+++ src/Bases/stubs/Object311.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include "../StageEntity.hpp"
 #include "../../AAA.hpp"
+#include "../../graphics/3d/modelanm.hpp"
 
 // MainProfileTable slot 311  |  ov009  |  profile @ 0x020db04c
-class Object311 : public StageEntity {
+class Object311 : public Actor {
 public:
+	Model model;
+	u8 _pad0[0x14];
 	static void *create();
 	inline Object311() {};
 	inline ~Object311() {};
