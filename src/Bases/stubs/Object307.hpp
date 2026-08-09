7dc78d3daf8753badf70a0155e80454f4bce4ef2
GlowingUmbreon <github@umbreon.cc>
2026-08-09 13:06:58 +0100

    Object307::create() - 100%

--- src/Bases/stubs/Object307.hpp
+++ src/Bases/stubs/Object307.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include "../StageEntity.hpp"
 #include "../../AAA.hpp"
+#include "../../graphics/3d/model.hpp"
 
 // MainProfileTable slot 307  |  ov009  |  profile @ 0x020dacdc
-class Object307 : public StageEntity {
+class Object307 : public Actor {
 public:
+	Model model;
+	u8 _pad[0x18];
 	static void *create();
 	inline Object307() {};
 	inline ~Object307() {};
