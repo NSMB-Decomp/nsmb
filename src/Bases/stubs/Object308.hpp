cf6892eb0b3733455d049c2cf756691c306e8ae2
GlowingUmbreon <github@umbreon.cc>
2026-08-09 13:10:03 +0100

    Object308::create() - 100%

--- src/Bases/stubs/Object308.hpp
+++ src/Bases/stubs/Object308.hpp
@@ -1,10 +1,18 @@
 #pragma once
 #include "../StageEntity.hpp"
 #include "../../AAA.hpp"
+#include "../../graphics/3d/model.hpp"
+
+class Object308_Model : public Model { // TODO: Is this in a seperate file?
+	public:
+	Object308_Model();
+};
 
 // MainProfileTable slot 308  |  ov009  |  profile @ 0x020daea0
-class Object308 : public StageEntity {
+class Object308 : public Actor {
 public:
+	Object308_Model model;
+	u8 _pad0[0x368];
 	static void *create();
 	inline Object308() {};
 	inline ~Object308() {};
