529030a9786817a246823d4c4ca549bc48aabc35
GlowingUmbreon <github@umbreon.cc>
2026-08-09 10:24:18 +0100

    Item::create() - 100%

---
+++ src/Bases/Item.cpp
@@ -0,0 +1,17 @@
+#include "Item.hpp"
+
+s32 Item::onCreate() {
+
+}
+s32 Item::onDestroy() {
+}
+
+Item *Item::create()
+{
+	return new Item();
+}
+
+ToadItem *ToadItem::create()
+{
+	return new ToadItem();
+}
---
+++ src/Bases/Item.hpp
@@ -0,0 +1,31 @@
+#include "../graphics//3d/model.hpp"
+#include "../graphics//3d/modelanm.hpp"
+#include "../nsmb_nitro.hpp"
+#include "StageEntity.hpp"
+
+class Item : public StageEntity
+{
+      public:
+	u32 _3f4;
+	ModelAnm modelAnm;
+	Model model;
+	u8 _pad0[0x18];
+	Vec3_32 _548;
+	Vec2_32 _558;
+	u8 _pad1[0x6c];
+
+	static Item *create();
+	inline Item() {};
+	inline ~Item() {};
+
+	virtual s32 onCreate();
+	virtual s32 onDestroy();
+};
+
+class ToadItem : public Item
+{
+      public:
+	static ToadItem *create();
+	inline ToadItem() {};
+	inline ~ToadItem() {};
+};
