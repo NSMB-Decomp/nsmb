f37dd168c977f1c27f65f4cbe4c095479dffc908
GlowingUmbreon <github@umbreon.cc>
2026-08-09 14:20:34 +0100

    Object325 - 100%

--- src/Bases/stubs/Object325.cpp
+++ src/Bases/stubs/Object325.cpp
@@ -1,8 +1,21 @@
 #include "Object325.hpp"
+extern void func_ov010_020e6524();
+extern void func_01ffded0();
 
 void *Object325::create()
 {
 	return new Object325();
+}
+
+s32 Object325::onRender() {
+	func_ov010_020e6524();
+	Nitro::func_01ff9010();
+	return true;
+}
+
+s32 Object325::onUpdate() {
+	func_01ffded0();
+	return true;
 }
 
 // 0x02123d58
--- src/Bases/stubs/Object325.hpp
+++ src/Bases/stubs/Object325.hpp
@@ -3,11 +3,14 @@
 #include "../../AAA.hpp"
 
 // MainProfileTable slot 325  |  ov010  |  profile @ 0x02123d58
-class Object325 : public StageEntity {
+class Object325 : public Actor {
 public:
 	static void *create();
 	inline Object325() {};
 	inline ~Object325() {};
+
+	virtual s32 onRender();
+	virtual s32 onUpdate();
 };
 
 extern ActorProfile Object325_Profile;
